0c240163bc7f9a3fc695b6a194bc179441b786a8
Jorge Palacios <reefaktor@gmail.com>
2022-02-05 17:55:06 -0300

    Improved movement

--- Assets/Soulseek/Scripts/PlayerController.cs
+++ Assets/Soulseek/Scripts/PlayerController.cs
@@ -1,48 +1,88 @@
+
 using System;
-using System.Net.NetworkInformation;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Soulseek
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private string layerPlatformName = "Platform";
+        [SerializeField] private string layerWallName = "Wall";
+        [SerializeField] private string layerItemName = "Item";
+        [Space(3)]
         [SerializeField] private Rigidbody2D rigidBody;
         [SerializeField] private float speed = 0.5f;
-        [SerializeField] private float jump = 100f;
+        [FormerlySerializedAs("jump")] [SerializeField] private float jumpForce = 100f;
+        [Space(3)] [SerializeField] private SpriteRenderer[] spriteRenderers;
         private Vector2 direction;
+        private bool oldJump;
+        private bool newJump;
         private int layerPlatform;
         private float mass;
 
-        private void Start()
+        private void Awake()
         {
             if (rigidBody == null)
             {
                 Debug.LogError("No rigidbody assigned");
             }
+        }
 
-            mass = rigidBody.mass;
-            layerPlatform = LayerMask.NameToLayer("Platform");
+        private void Start()
+        {
+            Initialize();
         }
 
         private void Update()
         {
-            direction.x = Input.GetAxis("Horizontal");
+            var horizontal = (int)Input.GetAxisRaw("Horizontal");
+            AdjustForward(horizontal, (int)direction.x);
+            direction.x = horizontal;
+            
             if (Input.GetButtonDown("Jump"))
             {
-                Debug.Log("JUMP!!!");
-                rigidBody.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+                rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
+        }
+        
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            
         }
 
-        private void LateUpdate()
+        private void OnCollisionExit2D(Collision2D other)
         {
-            direction.Normalize();
-            rigidBody.AddForce(direction * speed, ForceMode2D.Force);
-            if (direction.magnitude > 0f)
+        
+        }
+
+        private void FixedUpdate()
+        {
+            
+            direction.x = speed * Time.fixedDeltaTime * direction.x;
+            direction.y = rigidBody.velocity.y;
+
+            rigidBody.velocity = direction;
+        }
+
+        public void Initialize()
+        {
+            mass = rigidBody.mass;
+            layerPlatform = LayerMask.NameToLayer("Platform");
+            newJump = oldJump = false;
+        }
+
+        public void AdjustForward(int horizontalNew, int horizontalOld)
+        {
+            if (horizontalNew == horizontalOld) return;
+
+            var flipHorizontal = horizontalNew < 0 ? true : false;
+            foreach (var spriteRenderer in spriteRenderers)
             {
-                
+                spriteRenderer.flipX = flipHorizontal;
             }
         }
+        
     }
 }
